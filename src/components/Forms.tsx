5c6cef8c2f6b8185481095526b2e48d7005286f9
Maryam-Ashfaq10 <maryammashfaq000@gmail.com>
2024-05-05 23:18:26 +0500

    updated form

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { FormEvent } from "react"
 
 interface formState {
     user: {
@@ -20,25 +21,35 @@ const Forms = (): React.ReactNode => {
     })
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-       setState({
-        user:{
-            ...state.user, [e.target.name] : e.target.value
-        }
-       })
-       console.log(JSON.stringify(state.user))
+        setState({
+            user: {
+                ...state.user, [e.target.name]: e.target.value
+            }
+        })
+    }
+    const handleForm = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+    }
+
+    const handleInput = () => {
+        console.log(JSON.stringify(state.user))
     }
 
     return (
         <>
             <h1>LOGIN FORM</h1>
-            <form>
+            <form onSubmit={handleForm}>
 
-                username: <input type="text" value={state.user.username} onChange={handleChange} name="username" id="name" /> <br />
-                email:    <input style={{ margin: '5px' }} type="text" value={state.user.email} onChange={handleChange} name="email" id="email" /> <br />
-                password: <input type="password" value={state.user.password} onChange={handleChange} name="password" id="password" /><br />
-                phone number: <input style={{ margin: '5px' }} type="text" value={state.user.phone} onChange={handleChange} name="phone" id="phone" />
- 
-               <button type="submit">submit</button>
+                username:
+                <input type="text" value={state.user.username} onChange={handleChange} name="username" id="name" /> <br />
+                email:
+                <input style={{ margin: '5px' }} type="text" value={state.user.email} onChange={handleChange} name="email" id="email" /> <br />
+                password:
+                <input type="password" value={state.user.password} onChange={handleChange} name="password" id="password" /><br />
+                phone number:
+                <input style={{ margin: '5px' }} type="text" value={state.user.phone} onChange={handleChange} name="phone" id="phone" />
+
+                <button type="submit" onClick={handleInput}>submit</button>
             </form>
         </>
     )
