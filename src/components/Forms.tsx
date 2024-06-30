c64254cac8447e3b8a48e0afed2f8ff4fbd804cd
Maryam-Ashfaq10 <maryammashfaq000@gmail.com>
2024-06-30 17:22:09 +0500

    updated functionality

--- src/components/Forms.tsx
+++ src/components/Forms.tsx
@@ -1,39 +1,31 @@
 import { useState } from "react"
-import { FormEvent } from "react"
 
-interface formState {
-    user: {
-        username: string,
-        email: string,
-        password: string,
-        phone: number
-    }
-}
 
 const Forms = (): React.ReactNode => {
-    const [state, setState] = useState<formState>({
-        user: {
-            username: '',
-            email: '',
-            password: '',
-            phone: 0
-        }
-    })
+    const [userName, setUserName] = useState<string>('');
+    const [email, setEmail] = useState<string>('');
+    const [password, setPassword] = useState<string>('');
 
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
-        setState({
-            user: {
-                ...state.user, [e.target.name]: e.target.value
-            }
-        })
-    }
-    const handleForm = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
+    
+    const handleUserName = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const input= e.target.value;
+        setUserName(input)
+
     }
 
-    const handleInput = () => {
-        console.log(JSON.stringify(state.user))
+    const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const input= e.target.value;
+        setEmail(input)
+
     }
+
+    const handlePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const input= e.target.value;
+        setPassword(input)
+
+    }
+
+    const isDisabled = !userName || !email || !password
 
     return (
         <>
@@ -41,20 +33,20 @@ const Forms = (): React.ReactNode => {
             <form>
             <div className="mb-3 p-2">
                     <label htmlFor="exampleInputName" className="form-label">User Name</label>
-                    <input type="text" className="form-control" id="exampleInputName" />
+                    <input type="text" className="form-control" id="exampleInputName" value={userName} onChange={handleUserName} />
                 </div>
 
                 <div className="mb-3 p-2">
                     <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" />
+                    <input type="email" className="form-control" id="exampleInputEmail1" value={email} onChange={handleEmail} />
                 </div>
 
                 <div className="mb-3 p-2">
                     <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1"/>
+                    <input type="password" className="form-control" id="exampleInputPassword1" value={password} onChange={handlePassword}/>
                 </div>
                
-                <button type="submit" className="btn btn-primary m-3">Submit</button>
+                <button type="submit" className="btn btn-primary m-3" disabled={isDisabled}>Submit</button>
             </form>
         </>
     )
